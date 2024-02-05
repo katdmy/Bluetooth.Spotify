a554853a03c09c19c465678588bf8bbd588b5106
katdmy <dmykatkov@gmail.com>
2024-02-05 17:38:14 +1000

    Changed kotlin version to 1.9.22

--- app/src/main/java/com/katdmy/android/bluetoothreadermusic/receivers/NotificationBroadcastReceiver.kt
+++ app/src/main/java/com/katdmy/android/bluetoothreadermusic/receivers/NotificationBroadcastReceiver.kt
@@ -3,7 +3,6 @@ package com.katdmy.android.bluetoothreadermusic.receivers
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.util.Log
 
 class NotificationBroadcastReceiver(
     private var changeUseTTS: (Boolean) -> Unit,
